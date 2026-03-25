52eae9b16d4c27d6079fd02acb60ab8a9b85d514
jahphetB <japhetlakew098@gmail.com>
2026-03-25 14:57:20 -0600

    Apply EF Core migration

--- TaskManager.Api/Program.cs
+++ TaskManager.Api/Program.cs
@@ -79,6 +79,12 @@ builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 // --------------------
 // Middleware pipeline
 // --------------------
